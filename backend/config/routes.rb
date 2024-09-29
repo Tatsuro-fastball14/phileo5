edb6d2b43b4b0423bdb30d1c177b72a9b3ddc33f
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2024-09-29 23:38:19 +0900

    I want to display a list of stores on the screen.
    I want to display a list of stores on the screen.

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,57 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # devise_for :users
+  devise_for :admins
+  get 'places/index'
+  get 'cooks/search'
+  get 'members/new'
+  post 'orders/index'
+  post 'orders/pay'
+  post  'orders/destroy'
+  get  'orders/kiyaku'
+  get  'umarepos/new'
+  post  'cooks/new'
+  get 'users/mypage'
+  get 'users/show'
+  get 'users/edit'
+  get 'cooks/show'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  
+  
+
+  root to: "places#index"
+ 
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cooks  do
+    resources :umarepos do
+      resources :favorites
+    end
+  end
+
+  resources :users, only: [:show,:edit, :update, :destroy] do
+    get 'mypage', on: :collection # ユーザーのマイページへのルーティング
+  end
+
+  resources :orders, only: [:show, :create, :index,:destroy] do
+    collection do 
+      get 'order'
+      get  'pay'
+      
+    end
+  end
+  #ここでルーティング設定している
+  resources :videos
+  resources :cards
+  
+  resources :users do
+    member do
+      delete :destroy_account
+    end
+  end
+  
+   if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
 end
+
+end
+
