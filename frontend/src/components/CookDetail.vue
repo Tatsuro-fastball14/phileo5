4b950e7ad8355f9e0a2792d0c2d57b99e4dfe84f
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2024-11-17 22:31:10 +0900

    cook"detail.vue
    cook"detail.vue

---
+++ frontend/src/components/CookDetail.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="wrapper">
+    <h1>お店一覧</h1>
+    <ul>
+      <li v-for="cook in filteredCooks" :key="cook.id" class="cook-card">
+        <!-- カード全体をクリック可能にする -->
+        <nuxt-link :to="generateDetailURI(cook.id)" class="cook-link">
+          <div class="card-content">
+            <h2>{{ cook.store }}</h2>
+            <p>{{ cook.description }}</p>
+          </div>
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      cooks: [], // お店一覧データを格納する配列
+      searchQuery: '' // 検索キーワード
+    }
+  },
+  computed: {
+    filteredCooks() {
+      return this.cooks.filter((cook) =>
+        cook.store.toLowerCase().includes(this.searchQuery.toLowerCase())
+      )
+    }
+  },
+  mounted() {
+    this.fetchCooks()
+  },
+  methods: {
+    fetchCooks() {
+      axios
+        .get('http://localhost:3000/cooks')
+        .then((response) => {
+          this.cooks = response.data
+        })
+        .catch((error) => {
+          console.error('エラーが発生しました:', error)
+        })
+    },
+    generateDetailURI(cookId) {
+      return `/cooks/${cookId}` // 詳細ページのURIを生成
+    }
+  }
+}
+</script>
