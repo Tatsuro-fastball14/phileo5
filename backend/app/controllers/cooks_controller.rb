31872f9822e5dfe09be0c75fb698861952d51c3d
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2024-12-03 13:10:16 +0900

    render json:{cook:@cook, status:'create'}
     render json:{cook:@cook, status:'create'}

--- backend/app/controllers/cooks_controller.rb
+++ backend/app/controllers/cooks_controller.rb
@@ -13,7 +13,7 @@ class CooksController < ApplicationController
   def create
     @cook = Cook.new(cooks_params)
     if  @cook.save
-        render json:{cook:@cook, status:created}  
+        render json:{cook:@cook, status:'create'}  
     else
         render :new
     end
