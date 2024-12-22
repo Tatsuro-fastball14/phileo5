594f9d32443a40b6ef21ece75bab49aac9d6c327
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2024-12-22 09:25:54 +0900

    post  'cooks/umarepos'
    post  'cooks/umarepos'

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,20 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-  # devise_for :users
+  root to: 'cooks#index'
+ 
   get 'places/index'
   get 'cooks/search'
   get '/api/place', to: 'places#place'
   get '/api/test', to: 'application#test'
   post  'cooks/new'
-  post  'umarepos'
-  root to: 'cooks#index'
-  resources :cooks do
-    resources :umarepos, only:[:new,:create]
-end
+  post  'cooks/umarepos'
+  
   resources :cooks, only: [:index, :new, :create, :show] do
     collection do
       get 'search' # /cooks/search へのGETリクエストを設定
     end
+    resources :umarepos, only:[:new,:create]
   end
     resources :places, only: [:index, :create] do
     end
