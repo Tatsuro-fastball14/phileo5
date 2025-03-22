9235039f46901389bbc880a2f84f5903e132692b
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2025-03-22 09:23:51 +0900

    <div v-if="cook.images">       <img :src="cook.images" alt="店舗の画像" />
     <div v-if="cook.images">
          <img :src="cook.images" alt="店舗の画像" />

--- frontend/src/components/CookDetail.vue
+++ frontend/src/components/CookDetail.vue
@@ -14,8 +14,8 @@
       <p><strong>緯度:</strong> {{ cook.lat }}</p>
       <p><strong>経度:</strong> {{ cook.lng }}</p>
     </div>
-    <div v-if="cook.image">
-      <img :src="cook.image" alt="店舗の画像" />
+    <div v-if="cook.images">
+      <img :src="cook.images" alt="店舗の画像" />
     </div>
 
     <div v-if="umarepo">
