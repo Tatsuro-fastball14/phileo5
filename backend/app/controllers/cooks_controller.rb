9ccf4ef38c4bf0e2c9c9635168c3d74ff51764e1
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2025-03-25 06:36:04 +0900

    methods: [:images_url]
    methods: [:images_url]

--- backend/app/controllers/cooks_controller.rb
+++ backend/app/controllers/cooks_controller.rb
@@ -25,7 +25,7 @@ class CooksController < ApplicationController
     # render json: {
     #   cook_umarepo_output: @cook.as_json(include:  :umarepos)
     # }
-    render json:  @cook, include: :umarepos
+    render json:  @cook,methods: [:images_url], include: :umarepos
   end
 
   def search
